5bed435f9697dd9f0ae3a47cd64880a6dd10388d
TigerMax <genya-genya2008@hotmail.com>
2015-02-03 01:18:06 +0300

    v0.04 ИИ Тавра

---
+++ Проект/Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	public GameObject taur;
+	public GameObject you;
+	public AudioSource close;
+
+	// Use this for initialization
+	void Start () {
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(taur.transform.position,you.transform.position)<=0.5f) {close.Play();}
+	}
+}
