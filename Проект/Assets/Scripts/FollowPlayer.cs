7165340f95a4e29fe20700d43f7fccee8cf719d3
TigerMax <genya-genya2008@hotmail.com>
2015-02-03 15:30:57 +0300

    v0.1 Тест анимаций и ИИ

--- Проект/Assets/Scripts/FollowPlayer.cs
+++ Проект/Assets/Scripts/FollowPlayer.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour {
 	public GameObject player;
 	NavMeshAgent agent;
+	Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		agent=GetComponent<NavMeshAgent>();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		agent.destination = player.transform.position;
+		anim.SetInteger ("speed", 1);
+		rigidbody.transform.Translate (Vector3.forward * Time.deltaTime*3f);
 	
 	}
 }
