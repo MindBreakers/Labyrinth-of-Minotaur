e33d566344a9ae3bc2863ba580a76ae908f71a53
TigerMax <genya-genya2008@hotmail.com>
2015-02-05 01:02:07 +0300

    v0.11 Бродящий Тавр

---
+++ Проект/Assets/Scripts/BuoyInit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuoyInit : MonoBehaviour {
+	public Light glow;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (GetComponent<MeshRenderer> ());
+		Destroy (glow);
+		Destroy (this);
+	
+	}
+	
+
+}
---
+++ Проект/Assets/Scripts/TaurWandering.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaurWandering : MonoBehaviour {
+	public GameObject[] checkpoints;
+	public GameObject taur;
+	public GameObject Taurdest;
+
+	// Use this for initialization
+	void Start () {
+		Taurdest.transform.position = taurnextdest ().transform.position;
+	}
+	GameObject taurnextdest(){
+		int max;
+		int val;
+		Debug.Log ("Finddest");
+		max = checkpoints.Length;
+		val = Random.Range (0, max);
+		return checkpoints[val];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (taur.transform.position, Taurdest.transform.position) <= 2f) {
+			Taurdest.transform.position = taurnextdest ().transform.position;
+		}
+	
+	}
+}
